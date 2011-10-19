0db09e852faedff18486d6f567bc6219c0b2b60f
Phillip Kuhrt <phkuhrt@yahoo.de>
2011-10-19 17:28:00 +0200

    Code mit NetBeans formatiert

--- index.php
+++ index.php
@@ -1,37 +1,37 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="de">
-<head>
-<meta charset="utf-8" />
-<title>Der-Vertretungsplan</title>
-<link href="standart.css" rel="stylesheet" type="text/css" />
-<link href="style.css" rel="stylesheet" type="text/css" />
-</head>
-<body>
-	<header>
-    	<hgroup>
-        	<h1><a href="">Der-Vertretungsplan.de</a></h1>
-            <h2>Die bessere Alternative</h2>
-        </hgroup>
-    </header>
-    <nav class="border-radius nav">
-    	<ul>
-        	<li><a href="">Aktualisieren</a></li>
-            <li><a href="http://der-vertretungsplan.ietherpad.com/1" target="_blank">an der Entwicklung des Vertretungsplanes mitwirken!</a></li>
-            <li><a href="https://github.com/phi1010/der-vertretungsplan" target="_blank">GITHUB</a></li>
-		</ul>
-    </nav>
-	<section>
-    <table class="border-radius nav">
-		<?php include("vertretung.php"); ?>
-    </table>
-    </section>
-    <aside>
-		<?php include("meldungen.php"); ?>
-    </aside>
-    
-    <footer>
-    <p>Alle Angaben ohne Gew&auml;hr.</p>
-    <script src="http://logging.ourstats.de/js.php?ID=719764&style=blue" type="text/javascript"></script><noscript><a href="http://stats.ourstats.de/?ID=719764" target="_blank"><img src="http://logging.ourstats.de/logging.php?ID=719764&js=0&style=blue" border="0" /></a></noscript>
-     </footer>
-</body>
+    <head>
+        <meta charset="utf-8" />
+        <title>Der-Vertretungsplan</title>
+        <link href="standart.css" rel="stylesheet" type="text/css" />
+        <link href="style.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <header>
+            <hgroup>
+                <h1><a href="">Der-Vertretungsplan.de</a></h1>
+                <h2>Die bessere Alternative</h2>
+            </hgroup>
+        </header>
+        <nav class="border-radius nav">
+            <ul>
+                <li><a href="">Aktualisieren</a></li>
+                <li><a href="http://der-vertretungsplan.ietherpad.com/1" target="_blank">an der Entwicklung des Vertretungsplanes mitwirken!</a></li>
+                <li><a href="https://github.com/phi1010/der-vertretungsplan" target="_blank">GITHUB</a></li>
+            </ul>
+        </nav>
+        <section>
+            <table class="border-radius nav">
+                <?php include("vertretung.php"); ?>
+            </table>
+        </section>
+        <aside>
+            <?php include("meldungen.php"); ?>
+        </aside>
+
+        <footer>
+            <p>Alle Angaben ohne Gew&auml;hr.</p>
+            <script src="http://logging.ourstats.de/js.php?ID=719764&style=blue" type="text/javascript"></script><noscript><a href="http://stats.ourstats.de/?ID=719764" target="_blank"><img src="http://logging.ourstats.de/logging.php?ID=719764&js=0&style=blue" border="0" /></a></noscript>
+        </footer>
+    </body>
 </html>
--- vertretung.php
+++ vertretung.php
@@ -1,221 +1,189 @@
 <?php
+
 vertretungen();
-function vertretungen()
-{
-	$h2_start = "<td class = \"Ueberschrift\" colspan=\"8\"><h2>";
-	$h2_ende = "</h2></td>";
-	$i = 0;
-	$date = date("d.m.Y");
-	
-	//Einfügen nach Schlüsseln
-	
-	$pdate=timestamp(get_date("Mon"));//Datum als Datumsformat
-	$pdate2=getdate($pdate);
-	$content[$pdate2['year']][$pdate2['mon']][$pdate2['mday']] = array($pdate, parse("Mon"), "Montag"); //Timestamp als int, HTML, Name des Wochentags
-		
-	$pdate=timestamp(get_date("Tue"));
-	$pdate2=getdate($pdate);
-	$content[$pdate2['year']][$pdate2['mon']][$pdate2['mday']] = array($pdate, parse("Tue"), "Dienstag");
-	
-	$pdate=timestamp(get_date("Wed"));
-	$pdate2=getdate($pdate);
-	$content[$pdate2['year']][$pdate2['mon']][$pdate2['mday']] = array($pdate, parse("Wed"), "Mittwoch");
-	
-	$pdate=timestamp(get_date("Thu"));
-	$pdate2=getdate($pdate);
-	$content[$pdate2['year']][$pdate2['mon']][$pdate2['mday']] = array($pdate, parse("Thu"), "Donnerstag");
-	
-	$pdate=timestamp(get_date("Fri"));
-	$pdate2=getdate($pdate);
-	$content[$pdate2['year']][$pdate2['mon']][$pdate2['mday']] = array($pdate, parse("Fri"), "Freitag");
-	
-	unset($pdate);
-	unset($pdate2);
-	
-	//Nach Jahr sortieren, Keys auf 0,1,2,... setzen
-	ksort($content);
-	$years =  array_values($content);
-	
-	for($iy = 0; $iy < count($years); $iy++)
-	{
-		//Nach Monat sortieren, Keys auf 0,1,2,... setzen
-		ksort($years[$iy]);
-		$months = array_values($years[$iy]);
-		
-		for($im = 0; $im < count($months); $im++)
-		{
-			//Nach Tag sortieren, Keys auf 0,1,2,... setzen
-			ksort($months[$im]);
-			$days = array_values($months[$im]);
-			
-			for($id = 0; $id < count($days); $id++)
-			{
-				$cday = $days[$id];
-				$diff = dateDiffInDays(time(), $cday[0]);
-				if($diff >= 0)
-				{
-					//Tag ausgeben
-					print( $h2_start . $cday[2] . ", " . date("d.m.Y", $cday[0]) . " (" . getDayNumberString($diff) . ")" . $h2_ende );
-					display($cday[1]);
-					print( "<tr><td><br/><br/></tr></td>");
-				}
-			}
-		
-		}
-		
-	}
+
+function vertretungen() {
+    $h2_start = "<td class = \"Ueberschrift\" colspan=\"8\"><h2>";
+    $h2_ende = "</h2></td>";
+    $i = 0;
+    $date = date("d.m.Y");
+
+    //Einfügen nach Schlüsseln
+
+    $pdate = timestamp(get_date("Mon")); //Datum als Datumsformat
+    $pdate2 = getdate($pdate);
+    $content[$pdate2['year']][$pdate2['mon']][$pdate2['mday']] = array($pdate, parse("Mon"), "Montag"); //Timestamp als int, HTML, Name des Wochentags
+
+    $pdate = timestamp(get_date("Tue"));
+    $pdate2 = getdate($pdate);
+    $content[$pdate2['year']][$pdate2['mon']][$pdate2['mday']] = array($pdate, parse("Tue"), "Dienstag");
+
+    $pdate = timestamp(get_date("Wed"));
+    $pdate2 = getdate($pdate);
+    $content[$pdate2['year']][$pdate2['mon']][$pdate2['mday']] = array($pdate, parse("Wed"), "Mittwoch");
+
+    $pdate = timestamp(get_date("Thu"));
+    $pdate2 = getdate($pdate);
+    $content[$pdate2['year']][$pdate2['mon']][$pdate2['mday']] = array($pdate, parse("Thu"), "Donnerstag");
+
+    $pdate = timestamp(get_date("Fri"));
+    $pdate2 = getdate($pdate);
+    $content[$pdate2['year']][$pdate2['mon']][$pdate2['mday']] = array($pdate, parse("Fri"), "Freitag");
+
+    unset($pdate);
+    unset($pdate2);
+
+    //Nach Jahr sortieren, Keys auf 0,1,2,... setzen
+    ksort($content);
+    $years = array_values($content);
+
+    for ($iy = 0; $iy < count($years); $iy++) {
+        //Nach Monat sortieren, Keys auf 0,1,2,... setzen
+        ksort($years[$iy]);
+        $months = array_values($years[$iy]);
+
+        for ($im = 0; $im < count($months); $im++) {
+            //Nach Tag sortieren, Keys auf 0,1,2,... setzen
+            ksort($months[$im]);
+            $days = array_values($months[$im]);
+
+            for ($id = 0; $id < count($days); $id++) {
+                $cday = $days[$id];
+                $diff = dateDiffInDays(time(), $cday[0]);
+                if ($diff >= 0) {
+                    //Tag ausgeben
+                    print( $h2_start . $cday[2] . ", " . date("d.m.Y", $cday[0]) . " (" . getDayNumberString($diff) . ")" . $h2_ende);
+                    display($cday[1]);
+                    print( "<tr><td><br/><br/></tr></td>");
+                }
+            }
+        }
+    }
 }
 
-function getDayNumberString($numberOfDays)//int -> string
-{
-	switch($numberOfDays)
-		{
-			case 0:
-				return "Heute";
-				break;
-			case 1:
-				return "Morgen";
-				break;
-			case 2:
-				return "&Uuml;bermorgen";
-				break;
-			default:
-				return "In $numberOfDays Tagen";
-				break;
-		}
+function getDayNumberString($numberOfDays) {//int -> string
+    switch ($numberOfDays) {
+        case 0:
+            return "Heute";
+            break;
+        case 1:
+            return "Morgen";
+            break;
+        case 2:
+            return "&Uuml;bermorgen";
+            break;
+        default:
+            return "In $numberOfDays Tagen";
+            break;
+    }
 }
 
-function dateDiffInDays($a,$b)//date,date -> int
-{
-	//Durch das Addieren von 24h und Abrunden passiert folgendes:
-	//12h bis Mitternach: 1 Tag = Morgen
-	//Mitternacht: 1 Tag = Morgen
-	//Nach Mitternacht: 0 Tage = Heute
-	
-	//+1 nach dem Dividieren will ich nicht riskieren, das könnte einen BufferOverflow bei negativen Datumswerten ergeben.
-	
-	return floor(($b - $a + (24*60*60)) / (24*60*60));
+function dateDiffInDays($a, $b) {//date,date -> int
+    //Durch das Addieren von 24h und Abrunden passiert folgendes:
+    //12h bis Mitternach: 1 Tag = Morgen
+    //Mitternacht: 1 Tag = Morgen
+    //Nach Mitternacht: 0 Tage = Heute
+    //+1 nach dem Dividieren will ich nicht riskieren, das könnte einen BufferOverflow bei negativen Datumswerten ergeben.
+
+    return floor(($b - $a + (24 * 60 * 60)) / (24 * 60 * 60));
 }
 
-function timestamp($text)//string -> int
-{
-	//Quelle RegEx: http://www.php-resource.de/forum/showthread/t-13403.html
-	//Erst dt. Datum in en. Datum wandeln
-	return strtotime(preg_replace("#([0-9]{2})\.([0-9]{2})\.([0-9]{4})#","\\3-\\2-\\1",$text));
+function timestamp($text) {//string -> int
+    //Quelle RegEx: http://www.php-resource.de/forum/showthread/t-13403.html
+    //Erst dt. Datum in en. Datum wandeln
+    return strtotime(preg_replace("#([0-9]{2})\.([0-9]{2})\.([0-9]{4})#", "\\3-\\2-\\1", $text));
 }
 
-
-function get_date($day)
-{
-	$break = false;
-	$url = generate_url($day);
-	$file = @fopen($url,"r");
-	if ((@fopen($url,"r") != false))
-	{
-		while ((!feof($file)) && ($break == false))
-		{
-			$str = fgets($file);
-			if(strpos($str, "</h2>") != false)
-			{
-				$break = true;
-				fclose($file);
-				$pos = strripos($str,date("Y"))-6;
-				return substr($str,$pos,10);
-			}
-		}
-	}
-	else
-	{
-		return false;
-	}
-	return false;
+function get_date($day) {
+    $break = false;
+    $url = generate_url($day);
+    $file = @fopen($url, "r");
+    if ((@fopen($url, "r") != false)) {
+        while ((!feof($file)) && ($break == false)) {
+            $str = fgets($file);
+            if (strpos($str, "</h2>") != false) {
+                $break = true;
+                fclose($file);
+                $pos = strripos($str, date("Y")) - 6;
+                return substr($str, $pos, 10);
+            }
+        }
+    } else {
+        return false;
+    }
+    return false;
 }
 
-function parse($day)
-{
-	$break = false;
-	$i = 0;
-	$ersetzen = array("<td>" => "<td class = \"TabelleMitteilung\" colspan=\"8\">");
-	$url = generate_url($day);
-	$file = @fopen($url,"r");
-	if ((@fopen($url,"r") != false))
-	{
-		while ((!feof($file)) && ($break == false))
-		{
-			$str = fgets($file);
-			// parsen der Mitteilungen
-			if(stristr($str, "<center><table class=\"TabelleMitteilung\" caption=\"Mitteilung\">") != false)
-			{
-				$array[$i] = $str;
-				$i++;
-				$array[$i] = "<tr>";
-				$i++;
-				while(!feof($file) && (stristr($str, "</table>") == false))
-				{
-					$str = fgets($file);
-					$array[$i] = strtr($str, $ersetzen);
-					$i++;
-				}
-				$array[$i] = "</tr>";
-				$i++;
-			}
-			// parsen der Vertretungsplantabelle
-			if(stristr($str, "<table class=\"TabelleVertretungen\" cellpadding=\"2px\">") != false)
-			{
-				$array[$i] = $str;
-				$i++;
-				while(!feof($file) && (stristr($str, "</table>") == false))
-				{
-					$str = fgets($file);
-					$array[$i] = $str;
-					$i++;
-				}
-				$break = true;
-			}
-		}
-		$array[$i] = "<tr><td class = \"TabelleMitteilung\" colspan=\"8\">Quelle: <a href=\"$url\" target=\"_blank\">$url</a></td></tr>";
-		fclose($file);
-		return $array;
-	}
-	else
-	{
-		return false;
-	}
+function parse($day) {
+    $break = false;
+    $i = 0;
+    $ersetzen = array("<td>" => "<td class = \"TabelleMitteilung\" colspan=\"8\">");
+    $url = generate_url($day);
+    $file = @fopen($url, "r");
+    if ((@fopen($url, "r") != false)) {
+        while ((!feof($file)) && ($break == false)) {
+            $str = fgets($file);
+            // parsen der Mitteilungen
+            if (stristr($str, "<center><table class=\"TabelleMitteilung\" caption=\"Mitteilung\">") != false) {
+                $array[$i] = $str;
+                $i++;
+                $array[$i] = "<tr>";
+                $i++;
+                while (!feof($file) && (stristr($str, "</table>") == false)) {
+                    $str = fgets($file);
+                    $array[$i] = strtr($str, $ersetzen);
+                    $i++;
+                }
+                $array[$i] = "</tr>";
+                $i++;
+            }
+            // parsen der Vertretungsplantabelle
+            if (stristr($str, "<table class=\"TabelleVertretungen\" cellpadding=\"2px\">") != false) {
+                $array[$i] = $str;
+                $i++;
+                while (!feof($file) && (stristr($str, "</table>") == false)) {
+                    $str = fgets($file);
+                    $array[$i] = $str;
+                    $i++;
+                }
+                $break = true;
+            }
+        }
+        $array[$i] = "<tr><td class = \"TabelleMitteilung\" colspan=\"8\">Quelle: <a href=\"$url\" target=\"_blank\">$url</a></td></tr>";
+        fclose($file);
+        return $array;
+    } else {
+        return false;
+    }
 }
 
-function display($array)
-{
-	$array_length = count($array);
-	$i = 0;
-	while($array_length > $i)
-	{	if(!stripos($array[$i],"table") && !stripos($array[$i],"caption"))
-		{
-			echo utf8_encode($array[$i]);
-			$i++;
-		}
-		else
-		{
-			$i++;
-		}
-	}
+function display($array) {
+    $array_length = count($array);
+    $i = 0;
+    while ($array_length > $i) {
+        if (!stripos($array[$i], "table") && !stripos($array[$i], "caption")) {
+            echo utf8_encode($array[$i]);
+            $i++;
+        } else {
+            $i++;
+        }
+    }
 }
 
-function generate_url($day)
-{
-	$url = "http://asg-er.dyndns.org/vertretung/students/schuelerplan_";
-	switch ($day)
-	{
-		case "Mon":
-			return $url . "mo" . ".htm";
-		case "Tue":
-			return $url . "di" . ".htm";
-		case "Wed":
-			return $url . "mi" . ".htm";
-		case "Thu":
-			return $url . "do" . ".htm";
-		case "Fri":
-			return $url . "fr" . ".htm";
-	}
+function generate_url($day) {
+    $url = "http://asg-er.dyndns.org/vertretung/students/schuelerplan_";
+    switch ($day) {
+        case "Mon":
+            return $url . "mo" . ".htm";
+        case "Tue":
+            return $url . "di" . ".htm";
+        case "Wed":
+            return $url . "mi" . ".htm";
+        case "Thu":
+            return $url . "do" . ".htm";
+        case "Fri":
+            return $url . "fr" . ".htm";
+    }
 }
 
 ?> 
