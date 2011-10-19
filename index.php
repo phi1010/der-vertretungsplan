44e662ef823d23ef12d46155697286b4c9442d4a
staribxc <a7964836@nepwk.com>
2011-10-19 16:52:40 +0200

    Änderung des Links vom Pad von piratepad.net auf http://der-vertretungsplan.ietherpad.com/1 (wegen Erreichbarkeitsproblemen bei piratepad.net)

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     <nav class="border-radius nav">
     	<ul>
         	<li><a href="">Aktualisieren</a></li>
-            <li><a href="http://piratepad.net/S9HdHIJRYB" target="_blank">AN DER ENTWICKLUNG DES VERTRETUNGSPLANES MITWIRKEN!</a></li>
+            <li><a href="http://der-vertretungsplan.ietherpad.com/1" target="_blank">an der Entwicklung des Vertretungsplanes mitwirken!</a></li>
             <li><a href="https://github.com/phi1010/der-vertretungsplan">github</a></li>
 		</ul>
     </nav>
