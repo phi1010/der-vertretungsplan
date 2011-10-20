fbf5bdc6c76544235e6a4b3c9132a01839f4c641
Phillip Kuhrt <phkuhrt@yahoo.de>
2011-10-20 16:51:54 +0200

    Lite Version für Facebook

---
+++ lite.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="de">
+    <head>
+        <meta charset="utf-8" />
+        <title>Der-Vertretungsplan</title>
+        <link href="standard.css" rel="stylesheet" type="text/css" />
+        <link href="style.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <section>
+            <table class="border-radius nav">
+                <?php include("vertretung.php"); ?>
+            </table>
+        </section>
+        <footer>
+            <p>Alle Angaben ohne Gew&auml;hr.</p>
+            <script src="http://logging.ourstats.de/js.php?ID=719764&style=blue" type="text/javascript"></script>
+            <noscript>
+                <a href="http://stats.ourstats.de/?ID=719764" target="_blank">
+                    <img src="http://logging.ourstats.de/logging.php?ID=719764&js=0&style=blue" border="0" />
+                </a>
+            </noscript>
+        </footer>
+    </body>
+</html>
