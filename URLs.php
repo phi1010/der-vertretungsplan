48cb2675ba367ccc3d5725d926fcafbd76de5ff9
staribxc <a7964836@nepwk.com>
2011-10-24 22:04:18 +0200

    Url-Strings zusammenfassen

--- URLs.php
+++ URLs.php
@@ -7,15 +7,15 @@ function urlReplacements($day) {
     $url = "http://asg-er.dyndns.org/vertretung/students/schuelerplan_";
     switch ($day) {
         case "Mon":
-            return $url . "mo" . ".htm";
+            return $url . "mo.htm";
         case "Tue":
-            return $url . "di" . ".htm";
+            return $url . "di.htm";
         case "Wed":
-            return $url . "mi" . ".htm";
+            return $url . "mi.htm";
         case "Thu":
-            return $url . "do" . ".htm";
+            return $url . "do.htm";
         case "Fri":
-            return $url . "fr" . ".htm";
+            return $url . "fr.htm";
     }
 }
 ?>
