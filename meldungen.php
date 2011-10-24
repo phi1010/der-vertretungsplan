1fe90a4dbbdece06df64ca6e153bb85f85980e5c
staribxc <a7964836@nepwk.com>
2011-10-25 00:14:54 +0200

    Meldungen aus der Ranspalte entfernt, Termine übersichtlicher gestaltet.

--- meldungen.php
+++ meldungen.php
@@ -3,148 +3,16 @@
 meldungen_termine();
 
 function meldungen_termine() {
-    $switch = "termine";
-    $meldungen = "";
-    $termine = "";
+    $termine = "<p><span style=\"FONT-WEIGHT: bold\">Termine:</span></p>";
     $array = fill_array();
-    $i = 0;
-    $array_length = count($array);
+    $i = 10;
+    $array_length = count($array) - 4;
+
     while ($array_length > $i) {
         $char = $array[$i];
         $i++;
-        if (($char != "<") && ($array_length > $i)) {
-            switch ($switch) {
-                case "meldungen":
-                    $meldungen = $meldungen . $char;
-                    break;
-                case "termine":
-                    $termine = $termine . $char;
-                    break;
-            }
-        } else {
-            $char = $array[$i];
-            $i++;
-            switch ($char) {
-                case "p":
-                    switch ($switch) {
-                        case "meldungen":
-                            $meldungen = $meldungen . "<p>";
-                            $i = skip($array, $i, $array_length);
-                            break 2;
-                        case "termine":
-                            $termine = $termine . "<p>";
-                            $i = skip($array, $i, $array_length);
-                            break 2;
-                    }
-
-                case "b":
-                    $char = $array[$i];
-                    $i++;
-                    switch ($char) {
-                        case "r":
-                            switch ($switch) {
-                                case "meldungen":
-                                    $meldungen = $meldungen . "<br>";
-                                    $i = skip($array, $i, $array_length);
-                                    break 3;
-                                case "termine":
-                                    $termine = $termine . "<br>";
-                                    $i = skip($array, $i, $array_length);
-                                    break 3;
-                            }
-                        case ">";
-                            switch ($switch) {
-                                case "meldungen":
-                                    $meldungen = $meldungen . "<b>";
-                                    $i = skip($array, $i, $array_length);
-                                    break 3;
-                                case "termine":
-                                    $termine = $termine . "<b>";
-                                    $i = skip($array, $i, $array_length);
-                                    break 3;
-                            }
-                        default:
-                            $i = skip($array, $i, $array_length);
-                            break 2;
-                    }
-                case "a":
-                    switch ($switch) {
-                        case "meldungen":
-                            $meldungen = $meldungen . "<a target=\"_blank\" href=\"http://www.asg-er.de/";
-                            $i += 7;
-                            break 2;
-                        case "termine":
-                            $termine = $termine . "<a target=\"_blank\" href=\"http://www.asg-er.de/";
-                            $i += 7;
-                            break 2;
-                    }
-                case "t":
-                    $char = $array[$i];
-                    $i++;
-                    switch ($char) {
-                        case "d":
-                            switch ($switch) {
-                                case "meldungen":
-                                    $switch = "termine";
-                                    $i = skip($array, $i, $array_length);
-                                    break 3;
-                                case "termine":
-                                    $switch = "meldungen";
-                                    $i = skip($array, $i, $array_length);
-                                    break 3;
-                            }
-                        default:
-                            $i = skip($array, $i, $array_length);
-                            break 2;
-                    }
-                case "/":
-                    $char = $array[$i];
-                    $i++;
-                    switch ($char) {
-                        case "p":
-                            switch ($switch) {
-                                case "meldungen":
-                                    $meldungen = $meldungen . "</p>";
-                                    $i = skip($array, $i, $array_length);
-                                    break 3;
-                                case "termine":
-                                    $termine = $termine . "</p>";
-                                    $i = skip($array, $i, $array_length);
-                                    break 3;
-                            }
-                        case "b":
-                            switch ($switch) {
-                                case "meldungen":
-                                    $meldungen = $meldungen . "</b>";
-                                    $i = skip($array, $i, $array_length);
-                                    break 3;
-                                case "termine":
-                                    $termine = $termine . "</b>";
-                                    $i = skip($array, $i, $array_length);
-                                    break 3;
-                            }
-                        case "a":
-                            switch ($switch) {
-                                case "meldungen":
-                                    $meldungen = $meldungen . "</a>";
-                                    $i = skip($array, $i, $array_length);
-                                    break 3;
-                                case "termine":
-                                    $termine = $termine . "</a>";
-                                    $i = skip($array, $i, $array_length);
-                                    break 3;
-                            }
-                        default:
-                            $i = skip($array, $i, $array_length);
-                            break 2;
-                    }
-                default:
-                    $i = skip($array, $i, $array_length);
-                    break;
-            }
-        }
+        $termine = $termine . $char;
     }
-    echo ($meldungen) . "<br><br><br>";
     echo ($termine) . "<br><br><br>" . "Script Version: 0.1 BETA <br> Diese Seite befindet sich noch im Aufbau. Daher sind Darstellungsfehler und Fehlfunktionen nicht auszuschlie&szlig;en. Benutzer des Internet Explorers werden gebeten einen modernen Browser zu verwenden.";
 }
 
@@ -160,44 +28,53 @@ function skip($array, $i, $array_length) {
 
 function fill_array() {
     $break = false;
-    $i = 3;
+    $i = 0;
     $file = fopen("http://www.asg-er.de/", "r") or exit("Unable to open file!");
     while ((!feof($file) && ($break == false))) {
         $char = fgetc($file);
-        if ($char == "<") {
+        if ($char == ">") {
             $char = fgetc($file);
-            if ($char == "t") {
+            if ($char == "T") {
                 $char = fgetc($file);
-                if ($char == "a") {
-                    $array[0] = "<";
-                    $array[1] = "t";
-                    $array[2] = "a";
-                    while ((!feof($file) && ($break == false))) {
+                if ($char == "e") {
+                    $char = fgetc($file);
+                    if ($char == "r") {
                         $char = fgetc($file);
-                        $array[$i] = $char;
-                        $i++;
-                        if ($char == "<") {
+                        if ($char == "m") {
                             $char = fgetc($file);
-                            $array[$i] = $char;
-                            $i++;
-                            if ($char == "/") {
+                            if ($char == "i") {
                                 $char = fgetc($file);
-                                $array[$i] = $char;
-                                $i++;
-                                if ($char == "t") {
+                                if ($char == "n") {
                                     $char = fgetc($file);
-                                    $array[$i] = $char;
-                                    $i++;
-                                    if ($char == "a") {
-                                        $array[$i] = "b";
-                                        $i++;
-                                        $array[$i] = "l";
-                                        $i++;
-                                        $array[$i] = "e";
-                                        $i++;
-                                        $array[$i] = ">";
-                                        $break = true;
-                                        break;
+                                    if ($char == "e") {
+                                        $char = fgetc($file);
+                                        if ($char == "<") {
+                                            $char = fgetc($file);
+                                            while ((!feof($file) && ($break == false))) {
+                                                $char = fgetc($file);
+                                                $array[$i] = $char;
+                                                $i++;
+                                                if ($char == "<") {
+                                                    $char = fgetc($file);
+                                                    $array[$i] = $char;
+                                                    $i++;
+                                                    if ($char == "/") {
+                                                        $char = fgetc($file);
+                                                        $array[$i] = $char;
+                                                        $i++;
+                                                        if ($char == "t") {
+                                                            $char = fgetc($file);
+                                                            $array[$i] = $char;
+                                                            $i++;
+                                                            if ($char == "d") {
+                                                                $break = true;
+                                                                break;
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
                                     }
                                 }
                             }
@@ -207,6 +84,7 @@ function fill_array() {
             }
         }
     }
+
     fclose($file);
     return $array;
 }
