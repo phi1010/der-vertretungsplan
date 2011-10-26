466232815518e27b30ed31c87e60476755a6f0b9
Phillip Kuhrt <phkuhrt@yahoo.de>
2011-10-26 16:37:20 +0200

    Regex zur Korrektur der fehlenden "/" in "<meta>" und "<br>"

--- Parser.php
+++ Parser.php
@@ -1,22 +1,13 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html lang="de">
-    <head>
-        <meta charset="utf-8" />
-        <title>Der-Vertretungsplan</title>
-        <link href="standard.css" rel="stylesheet" type="text/css" />
-        <link href="style.css" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-        <?php
-        include("URLs.php");
-        echo parseReplacements(urlReplacements("Thu"));
+<?php
 
-        function parseReplacements($url) {
-            $filecontent = file_get_contents($url);
-            $doc = new DOMDocument();
-            $loadHTML = $doc->loadHTML($filecontent);
-            return var_dump($doc);
-        }
-        ?>
-    </body>
-</html>
+include("URLs.php");
+parseReplacements(urlReplacements("Thu"));
+
+function parseReplacements($url) {
+    $filecontent = file_get_contents($url);
+    $filecontent = preg_replace(array('#<meta(.*?)>#i', '#<br\s*>#i'), array('<meta$1/>', '<br/>'), $filecontent);
+    $doc = new DOMDocument();
+    $loadHTML = $doc->loadHTML($filecontent);
+    echo var_dump($doc);
+}
+?>
