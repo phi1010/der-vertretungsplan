03950d954fde1467f7e99b5050fdc2a6b66f1bc2
staribxc <a7964836@nepwk.com>
2011-10-26 21:54:49 +0200

    ungenutzte Funktionen entfernt, Parser beschleunigt (ca 0,1s)

--- meldungen.php
+++ meldungen.php
@@ -1,36 +1,12 @@
 <?php
 
-meldungen_termine();
+termine();
 
-function meldungen_termine() {
+function termine() {
     $termine = "<p><span style=\"FONT-WEIGHT: bold\">Termine:</span></p>";
-    $array = fill_array();
-    $i = 0;
-    $array_length = count($array)-4;
-
-    while ($array_length > $i) {
-        $char = $array[$i];
-        $i++;
-        $termine = $termine . $char;
-    }
-    echo ($termine) . "(Quelle: <a href=\"http://www.asg-er.de/\" target=\"_blank\">http://asg-er.de</a>)" . "</br></br>" . "<p>Script Version: 0.1 BETA <br> Diese Seite befindet sich noch im Aufbau. Daher sind Darstellungsfehler und Fehlfunktionen nicht auszuschlie&szlig;en. Benutzer des Internet Explorers werden gebeten einen modernen Browser zu verwenden.</p>";
-}
-
-function skip($array, $i, $array_length) {
-    $char = $array[$i - 1];
-
-    while (($char != ">") && ($array_length > $i)) {
-        $char = $array[$i];
-        $i++;
-    }
-    return $i;
-}
-
-function fill_array() {
     $break = false;
-    $start = false;
     $i = 3;
-    $file = fopen("http://www.asg-er.de/", "r") or exit("Unable to open file!");
+    $file = fopen("http://www.asg-er.de/", "r") or exit("Termine konnten nicht geladen werden.");
     while ((!feof($file) && ($break == false))) {
         $char = fgetc($file);
         if ($char == ">") {
@@ -51,41 +27,37 @@ function fill_array() {
                                         $char = fgetc($file);
                                         if ($char == "<") {
 
-                                            while ((!feof($file) && ($start == false))) {
+                                            while (!feof($file) && ($break == false)) {
                                                 $char = fgetc($file);
                                                 if ($char == "<") {
                                                     $char = fgetc($file);
                                                     if ($char == "p") {
                                                         $char = fgetc($file);
                                                         if ($char == ">") {
-                                                            $array[0] = "<";
-                                                            $array[1] = "p";
-                                                            $array[2] = ">";
-                                                            $start = true;
-                                                            break;
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                            while ((!feof($file) && ($break == false) && ($start == true))) {
-                                                $char = fgetc($file);
-                                                $array[$i] = $char;
-                                                $i++;
-                                                if ($char == "<") {
-                                                    $char = fgetc($file);
-                                                    $array[$i] = $char;
-                                                    $i++;
-                                                    if ($char == "/") {
-                                                        $char = fgetc($file);
-                                                        $array[$i] = $char;
-                                                        $i++;
-                                                        if ($char == "t") {
-                                                            $char = fgetc($file);
-                                                            $array[$i] = $char;
-                                                            $i++;
-                                                            if ($char == "d") {
-                                                                $break = true;
-                                                                break;
+                                                            $termine = $termine . "<p>";
+                                                            while ((!feof($file) && ($break == false))) {
+                                                                $char = fgetc($file);
+
+                                                                if (($char != "<") && (!feof($file))) {
+                                                                    $termine = $termine . $char;
+                                                                } else {
+                                                                    $char = fgetc($file);
+                                                                    if (($char != "/") && (!feof($file))) {
+                                                                        $termine = $termine . "<" . $char;
+                                                                    } else {
+                                                                        $char = fgetc($file);
+                                                                        if (($char != "t") && (!feof($file))) {
+                                                                            $termine = $termine . "</" . $char;
+                                                                        } else {
+                                                                            $char = fgetc($file);
+                                                                            if (($char != "d") && (!feof($file))) {
+                                                                                $termine = $termine . "</t" . $char;
+                                                                            } else {
+                                                                                $break = true;
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
                                                             }
                                                         }
                                                     }
@@ -101,9 +73,8 @@ function fill_array() {
             }
         }
     }
-
     fclose($file);
-    return $array;
+    echo ($termine) . "(Quelle: <a href=\"http://www.asg-er.de/\" target=\"_blank\">http://asg-er.de</a>)" . "</br></br>" . "<p>Script Version: 0.1 BETA <br> Diese Seite befindet sich noch im Aufbau. Daher sind Darstellungsfehler und Fehlfunktionen nicht auszuschlie&szlig;en. Benutzer des Internet Explorers werden gebeten einen modernen Browser zu verwenden.</p>";
 }
 
 ?>
