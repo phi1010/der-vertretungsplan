87ae6a51960b09e11ef34b2a23d19aa1c8296087
staribxc <a7964836@nepwk.com>
2011-10-26 22:06:29 +0200

    Menüleiste angepasse, IRC Webchat Link hinzugefügt

--- index.php
+++ index.php
@@ -15,9 +15,10 @@
         </header>
         <nav class="border-radius nav">
             <ul>
-                <li><a href="">Aktualisieren</a></li>
-                <li><a href="http://der-vertretungsplan.ietherpad.com/1" target="_blank">an der Entwicklung des Vertretungsplanes mitwirken!</a></li>
-                <li><a href="https://github.com/phi1010/der-vertretungsplan" target="_blank">GITHUB</a></li>
+                <li><a href="http://der-vertretungsplan.de/">Aktualisieren</a></li>
+                <li><a href="http://der-vertretungsplan.ietherpad.com/1" target="_blank">Mitwirken: Pad</a></li>
+                <li><a href="https://github.com/phi1010/der-vertretungsplan" target="_blank">Mitwirken: GITHUB</a></li>
+                <li><a href="http://webchat.esper.net/?channels=#vertretungsplan" target="_blank">IRC-Channel</a></li>
             </ul>
         </nav>
         <section>
