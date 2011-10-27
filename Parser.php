fd01f70bb4b8de2f217f52fcc39d804697f0ed8a
Phillip Kuhrt <phkuhrt@yahoo.de>
2011-10-27 20:48:43 +0200

    Schnelleres Regex zur Fehlerkorrektur, da "greedy"/"geizig", sodass das Ganze ohne Backtracking auskommt.

--- Parser.php
+++ Parser.php
@@ -5,7 +5,7 @@ parseReplacements(urlReplacements("Thu"));
 
 function parseReplacements($url) {
     $filecontent = file_get_contents($url);
-    $filecontent = preg_replace(array('#<meta(.*?)>#i', '#<br\s*>#i'), array('<meta$1/>', '<br/>'), $filecontent);
+    $filecontent = preg_replace(array('#<meta([^>]*)>#i', '#<br\s*>#i'), array('<meta$1/>', '<br/>'), $filecontent);
     $doc = new DOMDocument();
     $loadHTML = $doc->loadHTML($filecontent);
     echo var_dump($doc);
