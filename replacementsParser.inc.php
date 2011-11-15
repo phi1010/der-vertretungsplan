9511e35927b268944786ea50adeeb6e225763c56
Phillip Kuhrt <phkuhrt@yahoo.de>
2011-11-15 15:02:03 +0100

    Toleranz gegenüber Veränderungen an der Originalseite erhöht; Fehlermeldungen per echo mit Hinweis auf Originalseite. (cherry-picking from commit 4e8d447e6f18caae26f5ce91e7f6b6bbb0494327)

--- replacementsParser.inc.php
+++ replacementsParser.inc.php
@@ -51,20 +51,31 @@ function replacementsParser_parseURL($url) {
     //include 'tree.inc.php';
     //echo tree($html);
     $body = $html->getElementsByTagName('body')->item(0);
-
-    $header = $body->getElementsByTagName('center')->item(0)->getElementsByTagName('thead')->item(0);
-    $dateChanged = $header->getElementsByTagName('th')->item(2);
-    $res['DateChanged'] = replacementsParser_parseDateTime($dateChanged->textContent);
+    $tables = $body->getElementsByTagName('table');
 
     $title = $body->getElementsByTagName('h2')->item(0);
     $res['Date'] = replacementsParser_parseDate($title->textContent);
 
-    $notices = $body->getElementsByTagName('center')->item(1)->getElementsByTagName('table')->item(0)->getElementsByTagName('td')->item(0);
-    $res['Notices'] = replacementsParser_parseNotices($notices);
+    if ($tables->length >= 1) {
+        $header = $tables->item(0)->getElementsByTagName('thead')->item(0);
+        $dateChanged = $header->getElementsByTagName('th')->item(2);
+        $res['DateChanged'] = replacementsParser_parseDateTime($dateChanged->textContent);
 
-    $entries = $body->getElementsByTagName('center')->item(2)->getElementsByTagName('table')->item(0);
-    $res['Entries'] = replacementsParser_parseEntries($entries);
+        if ($tables->length >= 2) {
+            if ($tables->length >= 3) {
+                $notices = $tables->item(1)->getElementsByTagName('td')->item(0);
+                $res['Notices'] = replacementsParser_parseNotices($notices, $url);
 
+                $entries = $tables->item(2);
+                $res['Entries'] = replacementsParser_parseEntries($entries, $url);
+            } else {
+                $res['Notices'] = array();
+                
+                $entries = $tables->item(1);
+                $res['Entries'] = replacementsParser_parseEntries($entries, $url);
+            }
+        }
+    }
     return $res;
 }
 
@@ -73,9 +84,13 @@ function replacementsParser_parseURL($url) {
  * @param DOMElement $element
  * @return array
  */
-function replacementsParser_parseEntries($element) {
+function replacementsParser_parseEntries($element, $url) {
     $res = array();
     $first = true;
+    if ($element == null || $element == false) {
+        echo "<p>Fehler beim Parsen der Vertretungen. Bitte informieren sie sich unter auf der <a href=\"$url\">ASG - Website</a></p>\n";
+        return $res;
+    }
     foreach ($element->getElementsByTagName('tr') as $tr) {
         if ($first) {
             $first = false;
@@ -93,8 +108,12 @@ function replacementsParser_parseEntries($element) {
  * @param DOMElement $element
  * @return array 
  */
-function replacementsParser_parseNotices($element) {
+function replacementsParser_parseNotices($element, $url) {
     $res = array();
+    if ($element == null || $element == false) {
+        echo "<p>Fehler beim Parsen der Meldungen. Bitte informieren sie sich unter auf der <a href=\"$url\">ASG - Website</a></p>\n";
+        return $res;
+    }
     foreach ($element->childNodes as $notice) {
         if ($notice instanceof DOMText)
             if (!preg_match('/^[\s]*$/', $notice->wholeText))
@@ -129,7 +148,7 @@ function replacementsParser_parseDate($text) {
  * @return string 
  */
 function replacementsParser_prepareString($text) {
-    return /*utf8_encode*/($text);
+    return /* utf8_encode */($text);
     //return htmlspecialchars_decode(htmlentities($text, ENT_COMPAT, 'UTF-8'));
 }
 
