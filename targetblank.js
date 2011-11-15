745e9b353954f2cc06933f5fef630197302f3f3b
Phillip Kuhrt <phkuhrt@yahoo.de>
2011-11-15 19:21:20 +0100

    XHTML 1.0 Validated

---
+++ targetblank.js
@@ -0,0 +1,46 @@
+var ownurlpatt = /der-vertretungsplan\.de/;  // gegen Ihre eigene URL ersetzen!
+
+function addEvent(obj, evType, fn){
+  if (obj.addEventListener){
+    obj.addEventListener(evType, fn, false);
+    return true;
+  } else if (obj.attachEvent){
+    var r = obj.attachEvent("on"+evType, fn);
+    return r;
+  } else {
+    return false;
+  }
+}
+function getEventTarget(e){
+	//by Peter Paul Koch - http://www.quirksmode.org/js/events_compinfo.html#link7
+	return (e.target) ? e.target : e.srcElement
+}
+
+blankclick = function(e){
+	var tg = getEventTarget(e);
+	if ( tg.tagName.toLowerCase() == 'a' ) {
+		tg.target ='_blank';
+	} else if ( tg.parentNode.tagName.toLowerCase() == 'a' ) {
+		tg.parentNode.target ='_blank';
+	}
+	return true;
+}
+
+
+onPageLoad = function(){
+	var doclinks = document.getElementsByTagName("a");
+	for (var i = 0; i < doclinks.length; i++) {
+	//alert(doclinks[i].href);
+		if ( doclinks[i].rel == 'noblank' ) {
+			;
+		}
+		else if ( ! doclinks[i].href.match(ownurlpatt) && ! doclinks[i].href.match(/^(mailto|javascript):/) ) {
+		  if ( doclinks[i].attributes['title'] ) {
+		  	doclinks[i].attributes['title'].value =  "external link - new window: " + doclinks[i].attributes['title'].value;
+		  }
+		  addEvent(doclinks[i], "click", blankclick);
+		}
+	}
+}
+	
+window.onload = onPageLoad;
