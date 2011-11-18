037e972687d923e554ff9c67f4e5e92a5f49be85
phi1010 <phkuhrt@yahoo.de>
2011-11-18 16:54:30 +0100

    Hinweis auf automatische Updates.

--- index.php
+++ index.php
@@ -120,10 +120,10 @@
                     ?>
                 </p>
                 <br/>
-                <p>Dies ist eine inoffizielle Seite, die Inhalte von der <a href="http://asg-er.de">Originalhomepage</a> verwendet. Diese Seite wird nicht von der Schule betrieben. Alle Informationen werden ohne Gewähr zur Verfügung gestellt, für Fehler dieses Skripts kann keine Haftung übernommen werden. Aus Datenschutzgründen wurden die Namen der Lehrkräfte auf 4 Buchstaben gekürzt.</p>
+                <p>Dies ist eine inoffizielle Seite, die Inhalte automatisch von der <a href="http://asg-er.de">Homepage der Schule</a> abruft. Diese Seite wird nicht von der Schule betrieben. Alle Informationen werden ohne Gewähr zur Verfügung gestellt, für Fehler dieses Skripts kann keine Haftung übernommen werden. Aus Datenschutzgründen wurden die Namen der Lehrkräfte auf 4 Buchstaben gekürzt.</p>
                 <p>Hinweis der Schule: Gültig ist der aktuelle Aushang in der Schule. Der Onlineplan kann unter Umständen den Stand vom Vortag darstellen.</p>
                 <p></p>
-                <p>Haftungshinweis: Für den Inhalt extern verlinkter Seiten kann keine Haftung übernommen werden, es sind ausschließlich deren jeweilige Betreiber verantwortlich!</p>
+                <p>Haftungshinweis: Für den Inhalt extern verlinkter Seiten kann keine Haftung übernommen werden, es sind ausschließlich deren jeweilige Betreiber verantwortlich. Dies gilt insbesondere für Links, die automatisch von der Homepage der Schule übernommen würden.</p>
                 <p>Beim Besuch dieser Website werden IP-Adresse, Bildschirmgröße, Browserversion und ähnliche Informationen erfasst, um eine öffentliche Statistik zu erstellen.</p>
 
             </div>
