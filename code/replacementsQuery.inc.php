251bee82aa64b0b8009dc995a9b12ae27702b603
phi1010 <phkuhrt@yahoo.de>
2012-01-28 22:47:04 +0100

    Verwendung des APC Cache in replacementsQuery implementiert

--- code/replacementsQuery.inc.php
+++ code/replacementsQuery.inc.php
@@ -10,8 +10,8 @@ $urls = array(
     'Thu' => "http://asg-er.dyndns.org/vertretung/students/schuelerplan_do.htm",
     'Fri' => "http://asg-er.dyndns.org/vertretung/students/schuelerplan_fr.htm"
 );
-function replacementsQuery_getContents()
-{
+
+function replacementsQuery_getContents() {
     global $urls;
     $res = array();
     foreach ($urls as $key => $value) {
@@ -21,14 +21,35 @@ function replacementsQuery_getContents()
     }
     return $res;
 }
-function replacementsQuery_getURL($url)
-{
+
+function replacementsQuery_getURL($url) {
     if ($url == null || $url == '')
         return null;
-    $header = array();
+    
+    if (extension_loaded('apc')) {
+        $headernow = get_headers($url, 1);
+        $etagnow = $headernow['ETag'];
+        
+        $urldata = apc_fetch('replacementsQuery_'.$url);
+        if ($urldata != false) {
+            $etagold = $urldata['ETag'];
+            if ($etagnow == $etagold)
+                return $urldata['Text'];
+        }
+    }
+    
     $filecontent = @file_get_contents($url);
     if ($filecontent == false || $filecontent == null || $filecontent == '')
         return null;
+    
+    if (extension_loaded('apc')) {
+        echo "UPDATED replacementsQuery_$url";
+        apc_store('replacementsQuery_'.$url, array('ETag'=>$etagnow, 'Text'=>$filecontent),1*60*60);
+    }
+    
     return $filecontent;
 }
+
+
+
 ?>
